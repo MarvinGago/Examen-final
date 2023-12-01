193653f69d10b9377faadcd3c5d564485915a29d
MarvinGago <144975718+MarvinGago@users.noreply.github.com>
2023-12-01 13:13:06 -0600

    Add files via upload

---
+++ src/proyecto/Empleado.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto;
+
+/**
+ *
+ * @author CPUw1010
+ */
+public class Empleado {
+    private String nombre;
+    private int id;
+
+    public Empleado(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+    
+
---
+++ src/proyecto/JOptionPane.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto;
+
+/**
+ *
+ * @author CPUw1010
+ */
+class JOptionPane {
+
+    static void showMessageDialog(Object object, String toString) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static String showInputDialog(String ingrese_su_ID_de_empleado_para_confirmar_) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static int showOptionDialog(Object object, String seleccione_una_opción, String menú_Cine, Object object0, String[] opciones, String opcione) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
